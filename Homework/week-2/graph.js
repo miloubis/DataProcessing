ff7b475b1a5f4491e63e7aad58a969f2e6ecc1fa
miloubis <miloubisseling@gmail.com>
2017-02-17 23:29:45 +0100

    Update graph.js

--- Homework/week-2/graph.js
+++ Homework/week-2/graph.js
@@ -84,7 +84,6 @@ for (var i = 0; i < dates.length; i++) {
 window.onload = function() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d"); 
-    context.scale(0.756, 0.9);
 
     // DRAW X-AXIS
     context.beginPath();
