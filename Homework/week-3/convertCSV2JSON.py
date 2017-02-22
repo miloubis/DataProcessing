60bc25f5f12f3674b21c9fb34dddc4979f9f59cb
miloubis <miloubisseling@gmail.com>
2017-02-22 18:43:09 +0100

    Add existing file

--- Homework/week-3/convertCSV2JSON.py
+++ Homework/week-3/convertCSV2JSON.py
@@ -1,1 +1,23 @@
+# Name: Milou Bisseling
+# Studentnumber: 10427538
+'''
+	This program converts CSV to JSON
+'''
 
+import csv
+import json
+
+inputfile = 'data.csv'
+outputfile = 'data.json'
+fieldnames = ("Jaar","Aantal_meerlingen")
+
+csvfile = open(inputfile, 'r')
+jsonfile = open(outputfile, 'w')
+
+reader = csv.DictReader(csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+
+csvfile.close()
+jsonfile.close()
