d56d3759426e59c51127392d5b2f22233f1f82c4
miloubis <miloubisseling@gmail.com>
2017-02-23 10:46:46 +0100

    Add existing file

---
+++ Homework/week-3/barchart.js
@@ -0,0 +1,15 @@
+// Name: Milou Bisseling
+// Student number: 10427538
+
+// SYNCHRONOUS XMLHTTPREQUEST
+var myrequest = new XMLHttpRequest();
+var url = "https://github.com/miloubis/DataProcessing/blob/master/Homework/week-3/data.json";
+var alldata = null;
+myrequest.open("GET", url, false);
+myrequest.send();
+data = myrequest.responseText
+
+
+
+
+
